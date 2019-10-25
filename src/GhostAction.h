6e7f2ed040f1c678c54d9df46ddde1737be9615a
JRock007 <github@jean-romain.com>
2019-10-25 23:28:18 +0200

    Create possible actions fo Ghost

---
+++ src/GhostAction.h
@@ -0,0 +1,17 @@
+//
+//  GhostAction.h
+//  OctoJam2019
+//
+//  Created by Jean-Romain on 25/10/2019.
+//  Copyright © 2019 JustKodding. All rights reserved.
+//
+
+#pragma once
+
+typedef enum : int {
+    none,
+    interact,
+    shush,
+    dash,
+    fusrodah,
+} GhostAction;
