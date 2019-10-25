ca58486a6c3558a7be14f8e44804ccc0e4b19ca9
JRock007 <github@jean-romain.com>
2019-10-25 22:54:25 +0200

    Create Utils file

---
+++ src/Utils.h
@@ -0,0 +1,12 @@
+//
+//  Utils.h
+//  OctoJam2019
+//
+//  Created by Jean-Romain on 25/10/2019.
+//  Copyright © 2019 JustKodding. All rights reserved.
+//
+
+#pragma once
+#include <tuple>
+
+typedef std::tuple<float, float> Point;
