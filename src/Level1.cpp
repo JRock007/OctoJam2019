99c4f800a868a99c33f4d2e53c972ff2c0e9680f
JRock007 <github@jean-romain.com>
2019-10-25 23:07:02 +0200

    Create Level1

---
+++ src/Level1.cpp
@@ -0,0 +1,17 @@
+//
+//  Level1.cpp
+//  OctoJam2019
+//
+//  Created by Jean-Romain on 25/10/2019.
+//  Copyright © 2019 JustKodding. All rights reserved.
+//
+
+#include "Level1.hpp"
+
+void Level1::draw() {
+    
+}
+
+void Level1::update(float dt) {
+    
+}
---
+++ src/Level1.hpp
@@ -0,0 +1,16 @@
+//
+//  Level1.hpp
+//  OctoJam2019
+//
+//  Created by Jean-Romain on 25/10/2019.
+//  Copyright © 2019 JustKodding. All rights reserved.
+//
+
+#pragma once
+#include "Scene.hpp"
+
+class Level1: public Scene {
+public:
+    void draw();
+    void update(float dt);
+};
