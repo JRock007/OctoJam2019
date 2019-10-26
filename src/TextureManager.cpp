ba752365c75d6da622abd1aa6ea21fa92fc0b281
fabre_ale <alexis.fabre@supelec.fr>
2019-10-26 18:33:23 +0200

    TextureManager bufix

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,5 +1,7 @@
 #include "TextureManager.hpp"
 
+std::map<std::string, Texture2D> TextureManager::textures = std::map<std::string, Texture2D>();
+
 void TextureManager::loadTexture(std::string textureName, std::string texturePath)
 {
 	Texture2D texture = LoadTexture(texturePath.c_str());
