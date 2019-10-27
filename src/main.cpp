9317f982a57fc68ed6e60148b93ed6f8eab83b18
fabre_ale <alexis.fabre@supelec.fr>
2019-10-27 14:43:01 +0100

    Added shader ressources

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,23 @@
 #include "TextureManager.hpp"
 #include <ctime>
 
+#if defined(PLATFORM_DESKTOP)
+#define GLSL_VERSION            330
+#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
+#define GLSL_VERSION            100
+#endif
+
 int main(void)
 {
     // Initialization
 	std::srand((unsigned int)std::time(nullptr));
 
+	SetConfigFlags(FLAG_MSAA_4X_HINT);      // Enable Multi Sampling Anti Aliasing 4x (if available)
     auto window = Window();
     // window.toggleFullscreen();
     // window.setCursorEnabled(false);
+	const int screenWidth = window.getWidth();
+	const int screenHeight = window.getHeight();
 
 	TextureManager textureManager;
 	textureManager.loadTexture("tileset", "assets/Tileset_Prototype.png");
@@ -30,6 +39,13 @@ int main(void)
 //	Rectangle personSrc = { 5 * 16, 0, 4 * 16, 3 * 16 };
 //	Rectangle personDst = { 500, 300, scale * 4 * 16, scale * 3 * 16 };
 
+	// SHADERS
+	// std::cout << GLSL_VERSION << std::endl;
+	//Shader shader = LoadShader(0, FormatText("resources/shaders/glsl%i/fisheye.fs", GLSL_VERSION));
+	// Create a RenderTexture2D to be used for render to texture
+	// RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
+
+
     // Main game loop
     while (!WindowShouldClose()) {
 		ClearBackground(Color{ 106, 101, 118 });	
@@ -42,7 +58,11 @@ int main(void)
         BeginDrawing();
             // Draw nodes
             BeginMode2D(lvl.getCamera());
+			// BeginShaderMode(shader);
+
             lvl.draw();
+
+			// EndShaderMode();
             EndMode2D();
 
 			//DrawTexturePro(tileset, ghostSrc, ghostDst, Vector2{}, 0.f, WHITE);
