7eb8fb3af614a384547764d7eace7c13cbeff013
fabre_ale <alexis.fabre@supelec.fr>
2019-10-27 10:28:15 +0100

    Added spritesheet class

---
+++ src/Spritesheet.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "raylib.h"
+
+class Spritesheet
+{
+public:
+	Spritesheet(int frameW, int frameH, int imgW, int imgH);
+	Rectangle getSrcRect(int id);
+private:
+	int frameW;
+	int frameH;
+	int imgW;
+	int imgH;
+	int nbColumns;
+	int nbLines;
+};
